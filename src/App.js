c11a95748ad87bfaf5ddf88e214456b27c734b2e
Saheb Dhara <sahebdhara961@gmail.com>
2023-03-01 11:41:48 +0530

    s

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Alluser from "./components/Allusers"
 import Dilynote from "./components/Dailynotes"
 import EditUser from "./components/EditUser"
 import AddText from "./components/AddText"
+import MyComponent from "./components/spiner"
  //routers
 import {BrowserRouter, Routes, Route } from "react-router-dom"
+// import Signup from "./components/Resister/Resister.jsx"
 
 
 function App() {
   return (
     <BrowserRouter>
        <Navbar/>
+       {/* <MyComponent/> */}
        <Routes>
          <Route path="/" element= {<Dilynote/>}/>
-         <Route path="/all" element= {<Alluser/>}/>
+         <Route path="/all" element= {<MyComponent/>}/>
          <Route path="/add" element= {<Adduser/>}/>
          <Route path="/edit/:id" element= {<EditUser/>}/>
          <Route path="/text" element= {<AddText/>}/>
@@ -26,6 +29,10 @@ function App() {
          
         </Routes>
     </BrowserRouter>
+
+ 
+
+
   );
 }
 
---
+++ src/components/spiner.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
+// import { fetchDataFromMongoDB } from './api';
+import {allUser} from '../service/api'
+import Allusers from "./Allusers"
+
+const Preloader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Spinner = styled.div`
+  width: 64px;
+  height: 64px;
+  position: relative;
+
+  div {
+    position: absolute;
+    width: 5px;
+    height: 5px;
+    background: #333;
+    border-radius: 50%;
+    animation: ${keyframes`
+      0% {
+        top: 32px;
+        left: 32px;
+        width: 0;
+        height: 0;
+        opacity: 1;
+      }
+      100% {
+        top: 0;
+        left: 0;
+        width: 64px;
+        height: 64px;
+        opacity: 0;
+      }
+    `} 1.5s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  }
+
+  div:nth-child(2) {
+    animation-delay: -0.75s;
+  }
+`;
+
+const MyComponent = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await allUser();
+      setData(result);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <Preloader>
+          <Spinner>
+            <div></div>
+            <div></div>
+          </Spinner>
+        </Preloader>
+      ) : (
+        <div>
+          
+          <Allusers/>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MyComponent;
